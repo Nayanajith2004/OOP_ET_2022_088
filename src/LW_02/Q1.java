9a2d3aef5820595e309036962b6886cfca792808
Nayanajith2004 <tharindunayanajith2004@gmail.com>
2025-09-27 08:15:26 +0530

    lab worksheet 02(Q1,Q2,Q3)

--- src/LW_02/Q1.java
+++ src/LW_02/Q1.java
@@ -1,7 +1,12 @@
 package LW_02;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Random;
+
 public class Q1 {
     public static void main(String[] args) {
-
+        System.out.println("10 11 12 13 14 15 16 17 18 19\n"+"20 21 22 23 24 25 26 27 28 29\n"+"30 31 32 33 34 35 36 37 38 39\n"+"40 41 42 43 44 45 46 47 48 49");
     }
 }
---
+++ src/LW_02/Q6.java
@@ -0,0 +1,12 @@
+package LW_02;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Q6 {
+    public static void main(String[] args) {
+        Date today =new Date();
+        SimpleDateFormat sdf =new SimpleDateFormat("dd MMMM yyyy");
+        System.out.println(sdf.format(today));
+    }
+}
