43c00c70d8e8011c8182bdbe355aadd495826c03
Nayanajith2004 <tharindunayanajith2004@gmail.com>
2025-10-13 21:32:27 +0530

    LW_04_Q9 completed

--- src/LW_04/Q9.java
+++ src/LW_04/Q9.java
@@ -1,5 +1,20 @@
 package LW_04;
 
-public class Q9 {
+import java.util.Scanner;
 
+public class Q9 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter number of Rows");
+        int rows=scanner.nextInt();
+        for(int i=1;i<=rows;i++){
+            for(int k=0;k<(rows-i);k++){
+                System.out.print(" ");
+            }
+            for (int j=0;j<(2*i)-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
